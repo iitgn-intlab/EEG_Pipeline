de5d88788a07ff98d6cb549729e12cb688410257
cipherdoge <ritviksharma99@gmail.com>
2026-07-01 09:12:38 +0530

    Added bad channel handling and interpolation and a function for making epochs

--- Preprocessing.py
+++ Preprocessing.py
@@ -21,22 +21,43 @@ import logging
 logger = logging.getLogger('matplotlib.animation')
 logger.setLevel(logging.DEBUG)
 
-def preproc_main(raw, res_freq = 250, notch_freq = 60, l_filter = 1, h_filter = 100, reference = "average" auto_rem_ica = True, n_comp_ica = None, remove_labels = ["muscle artifact", "eye blink", "heart beat","line noise","channel noise"]):
+def preproc_main(raw, res_freq = 250, notch_freq = 60, l_filter = 1, h_filter = 100, reference = "average" auto_rem_ica = True, ica_method = "infomax" bad_channel = True, rem_bad_channel = False, interpolate_bad_channel = True, n_comp_ica = None, remove_labels = ["muscle artifact", "eye blink", "heart beat","line noise","channel noise"]):
     """
     This function preprocess a raw file with the following steps:
     1. Resampling.
     2. Notch filter.
     3. Bandpass filter.
     4. Sets reference.
-    5. Auto removes ICA components with mne_icalabel.
+    5. Removes bad channels and optionally interpolates them with spline interpolation.
+    6. Auto removes ICA components with mne_icalabel.
     It then returns the processed raw.
     """
     raw.resample(sfreq = res_freq)
     raw.notch_filter(freqs = [60], fir_design = "firwin")
     raw.filter(l_freq = l_filter, h_freq = h_filter)
     raw.set_eeg_reference(reference)
+    if bad_channel:
+        data = raw.get_data()
+        variances = np.var(data, axis=1)
+        mean_v = np.mean(variances)
+        std_v = np.std(variances)
+        threshold = 3 * std_v
+        bad_channels = [
+            raw_clean.ch_names[i]
+            for i in range(len(variances))
+            if abs(variances[i] - mean_v) > threshold
+        ]
+        print("Bad channels:",bad_channels)
+        raw_clean.info["bads"] = bad_channels
+        
+        if interpolate_bad_channel:
+            raw_clean.interpolate_bads(
+                reset_bads=True,
+                verbose=False
+            )
+            
     if auto_rem_ica:
-        ica = ICA(n_components=n_comp_ica, random_state=42, max_iter='auto',method="infomax",fit_params=dict(extended=True))  #Don't tweak this
+        ica = ICA(n_components=n_comp_ica, random_state=42, max_iter='auto',method = ica_method,fit_params=dict(extended=True))  #Don't tweak this
         ica.fit(raw)
         ic_labels = label_components(raw, ica, method="iclabel")                    #Don't change
         #print(ic_labels)
@@ -48,5 +69,16 @@ def preproc_main(raw, res_freq = 250, notch_freq = 60, l_filter = 1, h_filter = 
         raw = reconst_raw
     return raw
 
-        
-        
+def epocher(raw, duration = 10):
+    """
+    This function creates epochs of required duration.
+    """
+    epochs = mne.make_fixed_length_epochs(
+    raw,
+    duration=duration,
+    overlap=0.0,
+    preload=True,
+    verbose=False
+    )
+    return epochs        
+
